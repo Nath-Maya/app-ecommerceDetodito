76dccbdb1949a2d4f98a3ea6d8218d657c72266a
User <user@Users-MacBook-Pro-2.local>
2024-06-25 23:08:14 -0500

    Add screen Welcome

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Image, StyleSheet } from 'react-native';
 import Home from '../screens/Home';
-import { ROUTE } from './routes';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { ROUTE } from './Routes';
+import Welcome from '../screens/Welcome';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name={ROUTE.HOME} component={Home}/>
+      <Tab.Screen 
+      name={ROUTE.HOME}
+      component={Home}
+      options={{
+        tabBarIcon: () => (
+          <Image
+            source={require('../assets/icons/tab-navigation/hogar.png')}
+            style={styles.iconTab}
+          />
+        ),
+        title: 'Inicio',
+      }}
+      />
     </Tab.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  iconTab: {
+    width: 25,
+    height: 25
+  }
+})
---
+++ screens/Welcome.jsx
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity, Image} from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+export default function Welcome() {
+
+    const navigation = useNavigation();
+
+    const handlePress = () => {
+        navigation.navigate(''); 
+      };
+
+  return (
+    <SafeAreaView style={styles.safeAreaView}>
+        <Image source={require("../assets/icons/logo/logo-de-todito.png")} style={styles.image} />
+          <TouchableOpacity style={styles.button} onPress={handlePress}>
+            <Text style={styles.buttonText}>Ingresar</Text>
+          </TouchableOpacity>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+    safeAreaView: { 
+        alignItems: 'center',
+        flex: 1, 
+        justifyContent: 'center'
+    },
+    button : {
+        borderRadius: 15,
+        padding: 16,
+        marginTop: 20,
+        border: '2px'
+
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: '900'
+    },
+    image: {
+        width: 160,
+        height: 160 
+    }
+})
