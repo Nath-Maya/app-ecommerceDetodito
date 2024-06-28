060f36d01699fc7eb0ac22d23e49f1de554fc54a
User <user@Users-MacBook-Pro-2.local>
2024-06-28 14:31:33 -0500

    Create Orders structure
    Screen view, include route in tabNavigator and create orderItem component that renders the orders.

---
+++ components/OrderItem.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { formatDate } from '../utils/formatDate'
+import { formatPrice } from '../utils/formatPrice'
+
+export default function OrderItem({createdAt, totalPrice}) {
+  return (
+    <View style={styles.orderItem}>
+        <Text>{formatDate(createdAt)}</Text>
+        <Text>{formatPrice(totalPrice)}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    orderItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 16,
+      },
+})
--- screens/Orders.jsx
+++ screens/Orders.jsx
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 import React from 'react'
+import OrderItem from '../components/OrderItem'
+import orderData from "../data/orders.json"
 
 export default function Orders() {
   return (
-    <View>
-      <Text>Orders</Text>
+    <View style={styles.orders}>
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={orderData}
+        renderItem={({ item }) => <OrderItem {...item} />}
+        ListEmptyComponent={<Text>No orders</Text>}
+      />
     </View>
   )
 }
+
+export const styles = StyleSheet.create({
+  orders: {
+    backgroundColor: 'white',
+    minHeight: '100%',
+  },
+  list: {
+    gap: 32,
+  },
+})
