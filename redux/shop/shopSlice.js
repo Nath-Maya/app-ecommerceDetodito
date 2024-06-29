a6f56e0cb08af2afb4eb125c83d149a7996bed31
User <user@Users-MacBook-Pro-2.local>
2024-06-28 21:34:28 -0500

    Create shopSlice with define actions

---
+++ redux/shop/shopSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+import productsData from '../../data/products.json'
+import categoryData from '../../data/category.json'
+
+
+export const shopSlice = createSlice({
+    name: 'shop',
+  initialState: {
+    productsData,
+    categoryData,
+    categorySeleted: '',
+    productIdSelected: '',
+    productsFilteredByCategory: [],
+  },
+  reducers: {
+    setCategorySelected: (state, action) => {
+      state.categorySeleted = action.payload
+      state.productsFilteredByCategory = state.products.filter(
+        product => product.brand === action.payload
+      )
+    },
+    setProductIdSelected: (state, action) => {
+      state.productIdSelected = action.payload
+    },
+  },
+})
+
+export const { setCategorySelected, setProductIdSelected } = shopSlice.actions
+
+export default shopSlice.reducer
--- store/store.js
+++ store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from '../redux/cart/cartSlice'
+import shopReducer from '../redux/shop/shopSlice'
 
 const store = configureStore({
     reducer: {
         cart: cartReducer,
+        shop: shopReducer,
     }
 });
 
