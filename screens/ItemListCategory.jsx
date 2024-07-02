8d552374f2c4ae387ad81c3006d834619c0a3e54
User <user@Users-MacBook-Pro-2.local>
2024-07-02 12:37:59 -0500

    Fix bug in ItemListCategory

--- screens/ItemListCategory.jsx
+++ screens/ItemListCategory.jsx
@@ -7,36 +7,49 @@ import ProductItem from '../components/ProductItem';
 import SearchInput from '../components/SearchInput';
 import NotFoundModal from '../components/NotFoundModal';
 
-
-// Filtrar productos tanto por selección de categoría en lista o por busqueda de texto
-const filterProducts = (products, textToSearch, selectedCategory) => {
-  let filteredProducts = products || [];
-
-  if (selectedCategory) {
-    filteredProducts = filteredProducts.filter(product =>
-      product.category.toLowerCase() === selectedCategory.toLowerCase()
-    );
-  }
-
-  if (textToSearch) {
-    filteredProducts = filteredProducts.filter(product =>
-      product.title.toLowerCase().includes(textToSearch.toLowerCase())
-    );
-  }
-
-  return filteredProducts;
-};
-
 export default function ItemListCategory() {
+
+  // Filtrar productos tanto por selección de categoría en lista o por busqueda de texto
+  const filterProducts = (products = [], textToSearch, selectedCategory) => {
+    console.log('Initial products:', products);
+    let filteredProducts = Array.isArray(products) ? products : [];
+
+    if (selectedCategory) {
+      console.log('Filtering by category:', selectedCategory);
+      filteredProducts = filteredProducts.filter(product => {
+        console.log(`Comparing product category: ${product.category.toLowerCase()} with selected category: ${selectedCategory.toLowerCase()}`);
+        return product.category.toLowerCase() === selectedCategory.toLowerCase();
+      });
+      console.log('After category filter:', filteredProducts);
+    }
+
+    if (textToSearch) {
+      console.log('Filtering by text:', textToSearch);
+      filteredProducts = filteredProducts.filter(product =>
+        product.title.toLowerCase().includes(textToSearch.toLowerCase())
+      );
+      console.log('After text filter:', filteredProducts);
+    }
+
+    return filteredProducts;
+  };
+
   const [textToSearch, setTextToSearch] = useState(''); // Estado inicial para el texto que ingresa el usuario
   const [modalVisible, setModalVisible] = useState(false); // Estado de visibilidad de modal con mensaje de producto encontrado o no encontrado
   const navigation = useNavigation();
   const route = useRoute();
   const { category } = route.params;
-  const { data: products } = useGetProductsByCategoryQuery(category)
+  const { data: productsObject = {} } = useGetProductsByCategoryQuery(category); // Asegúrate de que `products` no sea undefined
   
+  // Convertir el objeto de productos en un arreglo
+  const products = Object.values(productsObject);
+
   // Filtrar productos basado en la categoría seleccionada y el texto de búsqueda
-  const productsFiltered = useMemo(() => filterProducts(products, textToSearch, category), [products, textToSearch, category]);
+  const productsFiltered = useMemo(() => {
+    const result = filterProducts(products, textToSearch, category);
+    console.log('Products filtered:', result);
+    return result;
+  }, [products, textToSearch, category]);
 
   // Efecto para mostrar modal si no se encuentran productos
   useEffect(() => {
