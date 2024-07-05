67b39e344857e95c6718f851f4de3f9a5bd58c22
User <user@Users-MacBook-Pro-2.local>
2024-07-05 00:23:56 -0500

    Delete screens deprecated

--- navigation/StackMyProfile.js
+++ navigation/StackMyProfile.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ROUTE } from './Routes';
-import ListAddress from '../screens/ListAddress';
 import MyProfile from '../screens/MyProfile';
-import LocationSelector from '../screens/LocationSelector';
 import ImageSelector from '../screens/ImageSelector';
 
 const Stack = createStackNavigator();
@@ -18,16 +16,6 @@ export default function StackMyProfile() {
         name={ROUTE.MY_PROFILE}
         component={MyProfile}
         options={{ headerTitle: 'Perfil' }}
-      />
-      <Stack.Screen
-        name={ROUTE.MY_LOCATION}
-        component={ListAddress}
-        options={{ headerTitle: 'Mi ubicación' }}
-      />
-      <Stack.Screen
-        name={ROUTE.LOCATION_SELECTOR}
-        component={LocationSelector}
-        options={{ headerTitle: 'Seleccionar ubicación' }}
       />
       <Stack.Screen
         name={ROUTE.IMAGE_SELECTOR}
